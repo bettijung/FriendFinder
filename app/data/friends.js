09a7c63af8793209cc9b23dbd4bae378b5869e76
Bettina Junghahn <bettinajunghahn@gmail.com>
2018-03-14 17:43:22 -0400

    front end

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,23 +2,38 @@
 // DATA
 var friendsArray = [
     {
-        name: "Lion King",
-        photo: "https://upload.wikimedia.org/wikipedia/commons/1/17/HansomeLion_002.jpg",
+        name: "Deidra",
+        photo: "https://images.unsplash.com/photo-1519716618908-02d0040b0f4b?ixlib=rb-0.3.5&s=5c549532f379ef643f3d3411e9979302&auto=format&fit=crop&w=700&q=60",
         scores: [5,4,5,3,4,5,4,3,5,1]
     },
     {
-        name: "Mr.Penguin",
-        photo: "http://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/istock-511366776.jpg?itok=cWhdWNZ8&resize=1100x619",
+        name: "Peter",
+        photo: "https://images.unsplash.com/photo-1520341280432-4749d4d7bcf9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ce73d2ef291206da938eaf4c43f584ef&auto=format&fit=crop&w=700&q=60",
         scores: [4,5,5,1,3,2,4,3,5,5]
     },
     {
-        name: "Mrs.Horse",
-        photo: "https://secure.aspca.org/files/aspca/social_images/advo-sideimage-us-horse-slaughter-safe-act-2015_062016_fb.jpg",
+        name: "Dan",
+        photo: "https://images.unsplash.com/photo-1507120878965-54b2d3939100?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4c16a2235cc17e78b037ad82ec3ab17c&auto=format&fit=crop&w=700&q=60",
         scores: [4,5,3,5,5,1,5,3,3,1]
     },
     {
-        name: "Mr.Owl",
-        photo: "https://youtopiaproject.com/wp-content/uploads/2015/08/Owl.jpg",
+        name: "Lindsey",
+        photo: "https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=214b5beac96f7d680a19b3836b28ccc3&auto=format&fit=crop&w=700&q=60",
+        scores: [5,3,1,2,2,3,1,5,4,3]
+    },
+    {
+        name: "Michael",
+        photo: "https://images.unsplash.com/photo-1504887823384-08d44627fb28?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=00b8ed24c096dcf88d939915c6a8662e&auto=format&fit=crop&w=700&q=60",
+        scores: [3,2,3,3,4,3,5,2,1,3]
+    },
+    {
+        name: "Trisha",
+        photo: "https://images.unsplash.com/photo-1518122663467-652b10d045d2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=12439fccfa86824ea9349fead27f55a7&auto=format&fit=crop&w=700&q=60",
+        scores: [3,4,5,3,3,2,3,4,3,1]
+    },
+    {
+        name: "Sarah",
+        photo: "https://images.unsplash.com/photo-1477118476589-bff2c5c4cfbb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ad57f7ca5832f3f72ee2c1c243c72f5c&auto=format&fit=crop&w=700&q=60",
         scores: [3,4,5,3,5,3,4,5,1,1]
     }
 ];
